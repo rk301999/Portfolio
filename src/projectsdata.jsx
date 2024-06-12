870a560683f6e2f5dd845bf835314bfc53034d96
ritesh kumar <42288787+rk301999@users.noreply.github.com>
2024-06-12 15:52:55 +0530

    Update projectsdata.jsx

--- src/projectsdata.jsx
+++ src/projectsdata.jsx
@@ -4,8 +4,8 @@ export const project =[
         "url":"booking.jpg",
         "title":"Booking Hotel ",
         "desc":"A modern app using React, Node.js, MongoDB, and Tailwind CSS. Offers real-time same-day hotel availability and bookings. Provides a responsive, user-friendly interface  for a seamless booking experience.",
-        "github":"https://github.com/rk301999?tab=repositories",
-        "demo":"https://github.com/rk301999?tab=repositories"
+        "github":"https://github.com/rk301999/Hotel_Booking_MERN",
+        "demo":"https://hotel-booking-mern-1-9066.onrender.com"
     },
     {
         "id":2,
@@ -13,7 +13,7 @@ export const project =[
         "title":"Food Cart",
         "desc":"A dynamic app built with React and Redux, featuring real-time updates to the cart. Enjoy a seamless food ordering experience with live inventory,realtime user online status check, for a responsive and user-friendly interface.",
         "github":"https://github.com/rk301999/React_Redux_tailwindFoodCart",
-        "demo":"https://riteshreactreduxtailwind.netlify.app/"
+        "demo":"https://riteshreactreduxtailwind.netlify.app"
     },
     {
         "id":3,
@@ -21,7 +21,7 @@ export const project =[
         "title":"Admin Panel For business",
         "desc":"A comprehensive admin panel built with React , Context API and material UI. It allows businesses to manage order statuses and track daily earnings in real-time. Features a user-friendly interface with dark mode support for enhanced usability and aesthetics. Ideal for efficient business operations management.",
         "github":"https://github.com/rk301999/AdminPanel_ReactMUI",
-        "demo":"https://reactadminreactmui.netlify.app/"
+        "demo":"https://reactadminreactmui.netlify.app"
     },
     {
         //coin page not working
@@ -30,7 +30,7 @@ export const project =[
         "title":"XCryto",
         "desc":"A leading crypto trading app built with React and Chakra UI. Provides real-time updates on top cryptocurrencies and their stats, offering a sleek, user-friendly interface for tracking and trading. Ideal for staying ahead in the global crypto market.",
         "github":"https://github.com/rk301999/XCrypto",
-        "demo":"https://riteshxcrypto.netlify.app/"
+        "demo":"https://riteshxcrypto.netlify.app"
     },
     {
         
@@ -39,7 +39,7 @@ export const project =[
         "title":"Game x VR",
         "desc":"An immersive app built with React, Context API, and Chakra UI. Features video streaming and an interactive sidebar that opens on click, offering a seamless, engaging gaming and VR experience with a modern and intuitive interface. Ideal for gamers seeking cutting-edge interaction.",
         "github":"https://github.com/rk301999/React_landing_Page2",
-        "demo":"https://reactlandingpage23.netlify.app/"
+        "demo":"https://reactlandingpage23.netlify.app"
     },
     
 ]
