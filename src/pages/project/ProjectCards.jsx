70f455b26a78f52c7b3a47ad1253f74349fbb44e
Ritesh <rk301999@gmail.com>
2024-06-12 16:02:30 +0530

    demo link fix project

--- src/pages/project/ProjectCards.jsx
+++ src/pages/project/ProjectCards.jsx
@@ -4,7 +4,7 @@ import {
   } from "react-icons/ai";
   import { PiPresentation } from "react-icons/pi";
 
-const ProjectCards = ({url,title,desc,link,github}) => {
+const ProjectCards = ({url,title,desc,demo,github}) => {
   return (
     <div className="card my-10 bg-transparent border-2 border-black shadow-purple-800 transform transition duration-500 hover:scale-90" style={{boxShadow:"0px 2px 26px 9px rgba(96,0,133,0.6)"}}>
         <div className="content w-[495px] h-[570px] text-white flex flex-col gap-4 items-center p-7">
@@ -16,7 +16,7 @@ const ProjectCards = ({url,title,desc,link,github}) => {
                     <div className='flex items-center gap-2 bg-purple-700 p-2 rounded-lg'><AiFillGithub/>{" "}Github</div>
                     
                 </a>
-                {link!="" && <a href={link}><div className='flex items-center gap-2 bg-purple-700 p-2 rounded-md'><PiPresentation/>{" "}Demo</div></a>}
+                {demo!="" && <a href={demo}><div className='flex items-center gap-2 bg-purple-700 p-2 rounded-md'><PiPresentation/>{" "}Demo</div></a>}
             </div>
         </div>
     </div>
